7d2e6d551e8da354d63b28a3682628d52504a1cf
Lucas Michon <wagashisakatsuki@gmailcom>
2023-04-25 16:50:57 +0200

    refactor: 💡 fix esLint issue and update readme

--- devOpsIsitech/src/App.jsx
+++ devOpsIsitech/src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import './App.css'
 import {Terminal} from './components/Terminal/Terminal'
 
--- devOpsIsitech/src/components/Terminal/Terminal.jsx
+++ devOpsIsitech/src/components/Terminal/Terminal.jsx
@@ -20,9 +20,7 @@ const Terminal = () => {
 
     return(
         <>
-            <form
-            id="form"
-             onSubmit={handleSubmit}>
+            <form id="form" onSubmit={handleSubmit}>
                 <input id="console-input" type="text" value={text} onChange={handleChange} autoFocus={true} />
                 <input type="submit" hidden={true}/>
             </form>
