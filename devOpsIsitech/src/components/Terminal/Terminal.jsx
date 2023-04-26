aa72375b6ab7930ac39735b2f97c1eed2cdb4c79
LouisMaupas <58144475+LouisMaupas@users.noreply.github.com>
2023-04-26 12:28:19 +0200

    feat: 🎸 add help & sayMyName features

--- devOpsIsitech/src/components/Terminal/Terminal.jsx
+++ devOpsIsitech/src/components/Terminal/Terminal.jsx
@@ -1,14 +1,15 @@
 import { useEffect, useState } from "react";
-// import {style} from './style.js';
+import {help, sayMyName} from "../../features/commands.js"
+import {focusInput} from "../../utils/misc.js"
 import './style.css'
 
 const Terminal = () => {
     const [text, setText] = useState(""),
         [textDisplayed, setTextDisplayed] = useState("");
 
-        useEffect(() => {
-        //    style();
-        })
+    useEffect(() => {
+        focusInput(document.querySelector('#console-input'))
+    }, [])
 
     const handleChange = (event) => {
         setText(event.target.value);
@@ -16,16 +17,33 @@ const Terminal = () => {
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        setTextDisplayed(text)
+        const arrayText = text.split(" "),
+            command = arrayText[0].toLowerCase(),
+            arg = arrayText[1];
+        switch(command){
+            case "help":
+                setTextDisplayed(help())
+                break;
+            case "saymyname":
+                setTextDisplayed(sayMyName(arg))
+                break;
+            default :
+            setTextDisplayed(help())
+        }
     }  
 
     return(
         <>
-            <form id="form" onSubmit={handleSubmit}>
-                <input id="console-input" type="text" value={text} onChange={handleChange} autoFocus={true} />
-                <input type="submit" hidden={true}/>
-            </form>
-            <span id="console-return">{textDisplayed}</span>
+            <div id="console">
+                <form id="form" onSubmit={handleSubmit}>
+                    <div id="console-user">
+                        $
+                        <input id="console-input" type="text" value={text} onChange={handleChange} autoFocus={true} />
+                    </div>
+                    <input type="submit" hidden={true}/>
+                </form>
+                <div id="console-return">{textDisplayed}</div> 
+            </div>
         </>
     )
 }
