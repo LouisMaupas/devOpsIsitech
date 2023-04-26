9b78b0cfdd57654169a161fce903db5d5ba78804
Lucas Michon <wagashisakatsuki@gmailcom>
2023-04-26 10:29:24 +0200

    test: 💍 fix terminal test (wip)

--- devOpsIsitech/tests/Terminal.test.js
+++ devOpsIsitech/tests/Terminal.test.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import { act } from 'react-dom/test-utils';
 import { Terminal } from '../src/components/Terminal/Terminal';
 
-describe('Terminal', () => {
+describe('Terminal Input', () => {
     test('it should display the input value', () => {
         const component = renderer.create(<Terminal />);
         const consoleInput = component.root.findByProps({ id: 'console-input' });
         const submit = component.root.findByType('form');
         const consoleReturn = component.root.findByProps({ id: 'console-return' });
 
-        consoleInput.props.onChange({ target: { value: 'Hello World' } });
-        submit.props.onSubmit({ preventDefault: () => {} });
+        act(() => {
+            consoleInput.props.onChange({ target: { value: 'Hello World' } });
+        });
+
+        act(() => {
+            submit.props.onSubmit({ preventDefault: () => {} });
+        });
 
         expect(consoleReturn.props.children).toBe('Hello World');
     });
