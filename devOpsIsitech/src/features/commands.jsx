3d992095f20c66bafc822739be2b6d2be824c1ca
Lucas Michon <wagashisakatsuki@gmailcom>
2023-04-27 15:55:43 +0200

    fix: 🐛 lint issue

--- devOpsIsitech/src/features/commands.jsx
+++ devOpsIsitech/src/features/commands.jsx
@@ -1,13 +1,13 @@
 const help = () => {
     return (
-    <div class="help">
-        <div><span class="fw-b">help :</span> affiche aide;</div>
-        <div><span class="fw-b">saymyname [name] :</span> affiche Hello [name] !;</div>
-        <div><span class="fw-b">lightmode :</span> change apparence en thème clair;</div> 
-        <div><span class="fw-b">darkmode :</span> change apparence en thème sombre;</div>
-        <div><span class="fw-b">ping :</span> ping google et retourne le délais;</div> 
-        <div><span class="fw-b">music :</span> lance une musique en arrière plan;</div> 
-        <div><span class="fw-b">music stop :</span> stop la musique qui est en cours.</div>
+    <div className="help">
+        <div><span className="fw-b">help :</span> affiche aide;</div>
+        <div><span className="fw-b">saymyname [name] :</span> affiche Hello [name] !;</div>
+        <div><span className="fw-b">lightmode :</span> change apparence en thème clair;</div> 
+        <div><span className="fw-b">darkmode :</span> change apparence en thème sombre;</div>
+        <div><span className="fw-b">ping :</span> ping google et retourne le délais;</div> 
+        <div><span className="fw-b">music :</span> lance une musique en arrière plan;</div> 
+        <div><span className="fw-b">music stop :</span> stop la musique qui est en cours.</div>
     </div>
     );
 };
